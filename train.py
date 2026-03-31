0c16f2c3bc7561ed32044e2a95365ed827fe7537
Shubhampanwar1 <shubhampanwar2006sp@gmail.com>
2026-03-31 17:31:37 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 
-# 1. Load the foundational Deep Learning brain
+# Load model
 model = YOLO('yolov8n.pt') 
 
-# 2. Define the training loop
+# defining trainig loop
 if __name__ == '__main__':
     print("Initiating Sentinel AI Training Sequence...")
-    
-    # 3. Start the mathematical training process
+
     results = model.train(
         data='C:/Users/HP/OneDrive/Documents/Minor project/Detection System/weapon-detection-v1.v12i.yolov8/data.yaml', # UPDATE THIS PATH
         epochs=50, 
